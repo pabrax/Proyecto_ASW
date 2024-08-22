5048b0b7552c85bf9d6065de0693a31ecd533cd2
pxbrx <pabloeg0420@outlook.com>
2024-08-22 11:22:46 -0500

    Add: Initial components

---
+++ frontend_angular/src/app/support-page/support-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-page.component.html',
+  styleUrl: './support-page.component.css'
+})
+export class SupportPageComponent {
+
+}
