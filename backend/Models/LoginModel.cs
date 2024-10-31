46c59d872312729f0b08175ba37a26f1e3fe5b29
pabrax <pabloeg0420@outlook.com>
2024-10-30 19:49:54 -0500

    Se ha cambiado el backend por el modelo generico aunque no ha sido probado

---
+++ backend/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+namespace ProyectoBackend.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Contrasena { get; set; } = string.Empty;
+    }
+}
+
+
