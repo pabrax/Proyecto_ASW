9ef7700db16113bdd35bc49fccc3e5da7aa76670
KevinAB06 <betancurpresigakevinandres@gmail.com>
2024-11-09 12:00:40 -0500

    Cambios 2

--- frontend_angular/src/app/components/aplication-header/aplication-header.component.ts
+++ frontend_angular/src/app/components/aplication-header/aplication-header.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-aplication-header',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './aplication-header.component.html',
-  styleUrl: './aplication-header.component.css'
+  styleUrl: './aplication-header.component.css',
+  providers:[AuthService]
 })
 export class AplicationHeaderComponent {
+  constructor(
+    private authservice:AuthService,
+    private router: Router 
+  ){
 
+  }
+  onSubmit(){
+    console.log('Cerrando sesion')
+    this.authservice.logout();
+    this.router.navigate(["/"]);
+  }
 }
--- frontend_angular/src/app/components/aplication-navbar/aplication-navbar.component.ts
+++ frontend_angular/src/app/components/aplication-navbar/aplication-navbar.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
-
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-aplication-navbar',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './aplication-navbar.component.html',
-  styleUrl: './aplication-navbar.component.css'
+  styleUrl: './aplication-navbar.component.css',
+  providers: [AuthService]
 })
 export class AplicationNavbarComponent {
+  constructor(
+    private authservice:AuthService,
+    private router: Router 
+  ){
 
+  }
+  onSubmit(){
+    this.authservice.logout();
+    this.router.navigate(["/"]);
+  }
 }
